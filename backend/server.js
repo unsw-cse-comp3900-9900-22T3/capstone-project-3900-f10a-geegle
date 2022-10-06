d29dd518d81c41fcdc2295415b6ee9e3ab641b4e
Tony Nguyen <nony.tguyen@gmail.com>
2022-10-07 00:46:37 +1100

    Added nodemon to dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
-import eventRouter from './routes/event.route';
+import eventRouter from './routes/event.route.js';
 
 const app = express();
 const PORT = 3000;
