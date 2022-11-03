232953a8c5a5a8bf0a23f40ade5af7ec71cc71f4
Kai Feng <z5160399@ad.unsw.edu.au>
2022-11-04 03:53:12 +1100

    seating plan changes colour based on availability and seat allocation style upgraded

--- frontend/src/components/DoltonHouse.jsx
+++ frontend/src/components/DoltonHouse.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
-const DoltonHouse = () => {
+const DoltonHouse = ({
+  unAvailSeats,
+  availSeats,
+  currentSelected
+}) => {
   const styles = {
     gridContainer: {
       border: "3px solid black",
       display: "grid",
       justifyContent: "center",
       alignItems: "center",
-      padding: "50px",
-      gridTemplateRows: "repeat(3, 1fr)",
+      padding: "20px",
+      gridTemplateRows: "repeat(2, 1fr)",
       gridTemplateColumns: "repeat(2, 1fr)",
       gap: "100px",
 
@@ -59,34 +63,58 @@ const DoltonHouse = () => {
       padding: "5px"
     }
   }
+  useEffect(()=> {
+    unAvailSeats.forEach((seat)=> {
+      const el = document.getElementById(`${seat.seatid}`);
+      el.style.backgroundColor = "#f76f6d";
+    })
+
+    availSeats.forEach((seat)=> {
+      const el = document.getElementById(`${seat.seatid}`);
+      el.style.backgroundColor = "#90EE90";
+    })
+    if (currentSelected !== '') {
+      const el = document.getElementById(`${currentSelected}`);
+      el.style.backgroundColor = "#55c1f3";
+    }
+  },[unAvailSeats,availSeats, currentSelected])
+
+ 
   return (
     <div id="grid container" style={styles.gridContainer}>
       <div id="table 1" style={styles.tableOne}>
-        <div className="seat" id="T1_1" style={styles.seat}>1</div>
-        <div className="seat" id="T1_2" style={styles.seat}>2</div>
-        <div className="seat" id="T1_3" style={styles.seat}>3</div>
+        <div className="label">Tabel 1</div>
+        <div className="label"></div>
+        <div className="label"></div>
+        <div className="seat" id="15" style={styles.seat}>1</div>
+        <div className="seat" id="16" style={styles.seat}>2</div>
+        <div className="seat" id="17" style={styles.seat}>3</div>
       </div>
       <div id="table 2" style={styles.tableTwo}>
-        <div className="seat" id="T2_1" style={styles.seat}>1</div>
-        <div className="seat" id="T2_2" style={styles.seat}>2</div>
-        <div className="seat" id="T2_3" style={styles.seat}>3</div>
+        <div className="label">Tabel 2</div>
+        <div className="label"></div>
+        <div className="label"></div>
+        <div className="seat" id="18" style={styles.seat}>1</div>
+        <div className="seat" id="19" style={styles.seat}>2</div>
+        <div className="seat" id="20" style={styles.seat}>3</div>
       </div>
       <div id="table 3" style={styles.tableThree}>
-        <div className="seat" id="T3_1" style={styles.seat}>1</div>
-        <div className="seat" id="T3_2" style={styles.seat}>2</div>
-        <div className="seat" id="T3_3" style={styles.seat}>3</div>
-      </div>
-      <div id="table 3" style={styles.tableThree}>
-        <div className="seat" id="T3_1" style={styles.seat}>1</div>
-        <div className="seat" id="T3_2" style={styles.seat}>2</div>
-        <div className="seat" id="T3_3" style={styles.seat}>3</div>
+        <div className="label">Tabel 3</div>
+        <div className="label"></div>
+        <div className="label"></div>
+        <div className="seat" id="21" style={styles.seat}>1</div>
+        <div className="seat" id="22" style={styles.seat}>2</div>
+        <div className="seat" id="23" style={styles.seat}>3</div>
       </div>
       <div id="table 4" style={styles.tableFour}>
-        <div className="seat" id="T3_1" style={styles.seat}>1</div>
-        <div className="seat" id="T3_2" style={styles.seat}>2</div>
-        <div className="seat" id="T3_3" style={styles.seat}>3</div>
-        <div className="seat" id="T3_4" style={styles.seat}>4</div>
-        <div className="seat" id="T3_5" style={styles.seat}>5</div>
+        <div className="label">Tabel 4</div>
+        <div className="label"></div>
+        <div className="label"></div>
+        <div className="seat" id="24" style={styles.seat}>1</div>
+        <div className="seat" id="25" style={styles.seat}>2</div>
+        <div className="seat" id="26" style={styles.seat}>3</div>
+        <div className="seat" id="27" style={styles.seat}>4</div>
+        <div className="seat" id="28" style={styles.seat}>5</div>
       </div>
     </div>
 
