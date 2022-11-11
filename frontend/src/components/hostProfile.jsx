6b91ddb2ceb97746fbb04c0788454b99f34552d2
Ni Feng <z5162408@unsw.edu.au>
2022-11-11 15:12:21 +1100

    finished host profiles

--- frontend/src/components/hostProfile.jsx
+++ frontend/src/components/hostProfile.jsx
@@ -24,21 +24,26 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 800,
+  height: '80vw',
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
+  overflow: 'scroll',
   p: 4,
 };
 const HostProfile=()=> {
 
-  const [open, setOpen] = React.useState(true);
-  const [hostInfo, setHostInfo] = React.useState({});
+  const [open, setOpen] = useState(true);
+  const [hostInfo, setHostInfo] = useState([]);
+  const [hostEvents, setHostEvents] = useState([]);
   const navigate = useNavigate();
   const {hostId} = useParams();
   const state = useLocation();
   const eventId = state.state;
   // console.log(eventObj);
+  console.log('eventId', eventId);
+  console.log('hostId', hostId);
 
   const handleClose = () => {
     navigate('/');
@@ -47,7 +52,8 @@ const HostProfile=()=> {
   };
 
   const fetchHostInfo = async() => {
-    const response = await fetch(`events/host/details/${hostId}`, {
+    console.log('here')
+    const response = await fetch(`http://localhost:3000/events/host/details/${hostId}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -56,15 +62,16 @@ const HostProfile=()=> {
     })
     const json = await response.json();
     setHostInfo(json)
+    setHostEvents(json.events);
     console.log(json)
   }
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetchHostInfo();
   }, [])
 
   return(
-<div>
+    <>
       <Modal
         open={open}
         onClose={handleClose}
@@ -80,11 +87,71 @@ const HostProfile=()=> {
             Host Name: {hostInfo.hostName}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Host Rating: {hostInfo.hostRating.toFixed(2)}
+            Host Rating: {hostInfo.hostRating}
           </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Host Email: {hostInfo.hostEmail}
+          </Typography>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Host Event Rating Breakdown:
+          </Typography>
+          {hostEvents.map((obj,idx) => {
+            return(
+              <Card key={idx} sx={{ maxWidth: '100%' ,display: 'grid'}}>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                  {obj.eventName}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                  {`Start Date: ${obj.startDateTime.substring(0, 10)} End Date: ${obj.endDateTime.substring(0, 10)}`}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                  {`Event Venue: ${obj.eventVenue}`}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                  {`Number Of Reviews: ${obj.numReviews}`}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                  {`Event Score: ${obj.eventScore}`}
+                  </Typography>
+                  {Array(Math.ceil(obj.eventScore))
+                    .fill(0)
+                    .map((_, i) => (
+                      <svg
+                        key={i}
+                        height="35"
+                        width="35"
+                        aria-label="coloured star rating"
+                      >
+                        <polygon
+                          points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
+                          fill="#ffd800"
+                        />
+                      </svg>
+                    ))}
+                  {Array(5 - Math.ceil(obj.eventScore))
+                    .fill(0)
+                    .map((_, i) => (
+                      <svg
+                        key={i}
+                        height="35"
+                        width="35"
+                        aria-label="uncoloured star rating"
+                      >
+                        <polygon
+                          points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
+                          fill="#7e7e7e"
+                          stroke="#7e7e7e"
+                          strokeWidth="1"
+                        />
+                      </svg>
+                    ))}
+                </CardContent>
+              </Card> 
+          )})}
         </Box>
       </Modal>
-    </div>
+    </>
   );
 };
 export default HostProfile;
