358a1c3d29b5fa6e1134434e63e0ae23edde20ae
hay-it-me <haydenou98@gmail.com>
2022-11-14 14:11:10 +1100

    fix hostevents

--- backend/services/event.service.js
+++ backend/services/event.service.js
@@ -323,7 +323,7 @@ export const getHostEventsService = async(req, res) => {
         let upcomingEventList = [];
         for (let i = 0; i < eventList.length; i++) {
             const seating = await isSeatedEventDb(eventList[i].eventid)
-            const reviewRating = await eventRatingScore(eventList[i].eventid)
+            const reviewRating = await eventRatingScore(eventList[i].eventid, req.userID)
             
             upcomingEventList.push({
                 eventID: eventList[i].eventid,
